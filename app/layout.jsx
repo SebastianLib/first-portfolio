43243155eef03967afd77f6c4560bc8b025c1479
SebastianLib <sebastianlib04@gmail.com>
2024-01-21 00:03:26 +0100

    add metadata to page.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,13 +9,6 @@ const poppins = Poppins({
   variable: "--font-poppins",
 });
 
-export const metadata = {
-  title: 'SeliCode - tworzenie stron internetowych',
-  description: 'Projektowanie i tworzenie najwyższej jakości stron internetowych, będące świetną wizytówką twojej firmy.',
-  icons: {
-    icon: "/assets/logoSvg.svg",
-  },
-}
 
 export default function RootLayout({ children }) {
 
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,13 @@ import Contact from '@/components/Contact'
 import Hero from '@/components/Hero'
 import Offer from '@/components/Offer'
 import { Toaster } from "react-hot-toast";
-
+export const metadata= {
+  title: 'SeliCode - tworzenie stron internetowych',
+  description: 'Projektowanie i tworzenie najwyższej jakości stron internetowych, będące świetną wizytówką twojej firmy.',
+  icons: {
+    icon: "/assets/logoSvg.svg",
+  },
+}
 export default function Home() {
   return (
     <>
